d9735c3abf060504b12f55bd2fd96b292a2fd9bf
YangTaeyoung <59782504+YangTaeyoung@users.noreply.github.com>
2025-02-17 20:32:05 +0900

    for test action

---
+++ math/math_test.go
@@ -0,0 +1,47 @@
+package math
+
+import "testing"
+
+func TestDivide(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "10 나누기 2",
+			args: args{
+				a: 10,
+				b: 2,
+			},
+			want:    5,
+			wantErr: false,
+		},
+		{
+			name: "10 나누기 0",
+			args: args{
+				a: 10,
+				b: 0,
+			},
+			want:    0,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Divide(tt.args.a, tt.args.b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Divide() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Divide() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
